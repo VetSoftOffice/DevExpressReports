c1b78c9d88bd69001e332d6d9ec9688875575437
mezz@vetsoft.com.eg <mezz@vetsoft.com.eg>
2025-05-21 00:57:10 +0300

    create init weekly prod perf

---
+++ Reports/Farms/Layer/ProductionPeriod/Performance/SR_LAY_WeeklyProdPerf/SR_LAY_WeeklyProdPerf.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.XtraReports.UI;
+
+namespace DevExpressReports.Reports.Farms.Layer.ProductionPeriod.Performance.SR_LAY_WeeklyProdPerf
+{
+    public partial class SR_LAY_WeeklyProdPerf : XtraReport
+    {
+        public SR_LAY_WeeklyProdPerf()
+        {
+            InitializeComponent();       
+        }
+    }
+}
