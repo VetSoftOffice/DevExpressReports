3351a3145d9f30561aeda77ede5e20cac75367bd
mezz@vetsoft.com.eg <mezz@vetsoft.com.eg>
2025-06-22 07:01:44 +0300

    incomplete work on daily stock design

---
+++ Reports/Farms/Layer/ProductionPeriod/EggProduction/SR_LAY_DailyStockReport/SR_LAY_DailyStockReport.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+
+namespace DevExpressReports.Reports.Farms.Layer.ProductionPeriod.EggProduction.SR_LAY_DailyStockReport
+{
+    public partial class SR_LAY_DailyStockReport : XtraReport
+    {
+        public SR_LAY_DailyStockReport()
+        {
+            InitializeComponent();
+        }
+        //-----------------------------------------------------------------------------------------
+        
+        //-----------------------------------------------------------------------------------------
+        
+        //-----------------------------------------------------------------------------------------
+    }
+}
