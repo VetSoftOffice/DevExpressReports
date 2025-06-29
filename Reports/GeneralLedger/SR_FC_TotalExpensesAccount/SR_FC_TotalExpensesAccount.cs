849030f250e55e82310a45a2079fbc776a87894b
VetSoft21 <vetsoft21@hotmail.com>
2025-06-29 11:23:23 +0300

    Creating

---
+++ Reports/GeneralLedger/SR_FC_TotalExpensesAccount/SR_FC_TotalExpensesAccount.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace DevExpressReports.Reports.GeneralLedger.SR_FC_TotalExpensesAccount
+{
+    public partial class SR_FC_TotalExpensesAccount : DevExpress.XtraReports.UI.XtraReport
+    {
+        public SR_FC_TotalExpensesAccount()
+        {
+            InitializeComponent();
+        }
+    }
+}
